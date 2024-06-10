b7d9950cb3b7ad5ac21b773a121f19613799ef0b
kot_lukasz <lukaszkot95@gmail.com>
2024-06-10 14:31:45 +0200

    injecting topology and topics

--- order-app/src/main/java/com/example/orderapp/topology/OrdersTopology.java
+++ order-app/src/main/java/com/example/orderapp/topology/OrdersTopology.java
@@ -1,11 +1,13 @@
 package com.example.orderapp.topology;
 
+import com.example.orderapp.domain.Order;
 import com.example.orderapp.serdes.SerdesFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Printed;
 
 @Slf4j
 public class OrdersTopology {
@@ -17,6 +19,9 @@ public class OrdersTopology {
         StreamsBuilder streamsBuilder = new StreamsBuilder();
 
         var ordersStream = streamsBuilder.stream(ORDERS, Consumed.with(Serdes.String(), SerdesFactory.orderSerdes()));
+
+        ordersStream.print(Printed.<String, Order>toSysOut()
+                .withLabel("orders"));
 
         return streamsBuilder.build();
     }
