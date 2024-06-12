af84f39224be0a2629f57079ad871b320eecff2a
kot_lukasz <lukaszkot95@gmail.com>
2024-06-12 22:36:34 +0200

    splitting kafka stream into two different branches for general orders and restaurant orders to write to different topics

--- order-app/src/main/java/com/example/orderapp/topology/OrdersTopology.java
+++ order-app/src/main/java/com/example/orderapp/topology/OrdersTopology.java
@@ -1,20 +1,26 @@
 package com.example.orderapp.topology;
 
 import com.example.orderapp.domain.Order;
+import com.example.orderapp.domain.OrderType;
 import com.example.orderapp.serdes.SerdesFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.Printed;
+import org.apache.kafka.streams.kstream.*;
 
 @Slf4j
 public class OrdersTopology {
     public static final String ORDERS = "orders";
     public static final String STORES = "stores";
 
+    public static final String GENERAL_ORDERS = "general_orders";
+    public static final String RESTAURANT_ORDERS = "restaurant_orders";
+
     public static Topology buildTopology(){
+
+        Predicate<String, Order> generalPredicate = (string, order) -> order.orderType().equals(OrderType.GENERAL);
+        Predicate<String, Order> restaurantPredicate = (string, order) -> order.orderType().equals(OrderType.RESTAURANT);
 
         StreamsBuilder streamsBuilder = new StreamsBuilder();
 
@@ -22,6 +28,18 @@ public class OrdersTopology {
 
         ordersStream.print(Printed.<String, Order>toSysOut()
                 .withLabel("orders"));
+
+        ordersStream.split(Named.as("general-restaurant"))
+                .branch(generalPredicate, Branched.withConsumer(generalOrderStream -> {
+                            generalOrderStream.print(Printed.<String, Order>toSysOut().withLabel("generalStream"));
+                            generalOrderStream.to(GENERAL_ORDERS, Produced.with(Serdes.String(), SerdesFactory.orderSerdes()));
+                        })
+                )
+                .branch(restaurantPredicate, Branched.withConsumer(restaurantOrderStream -> {
+                            restaurantOrderStream.print(Printed.<String, Order>toSysOut().withLabel("restaurantStream"));
+                            restaurantOrderStream.to(RESTAURANT_ORDERS, Produced.with(Serdes.String(), SerdesFactory.orderSerdes()));
+                        })
+                );
 
         return streamsBuilder.build();
     }
